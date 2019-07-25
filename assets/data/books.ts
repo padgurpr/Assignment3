244142ee29cabea5e46b9e4862cfb9c3b2413605
padgurpr <52726352+padgurpr@users.noreply.github.com>
2019-07-25 17:28:59 -0400

    Add files via upload

---
+++ assets/data/books.ts
@@ -0,0 +1,9 @@
+
+import {books} from '../../app/bookInfo';
+export const MYBOOKS :books[]=
+[
+    {bname:"Mind Platter ", bauthor:"Najwa Zebain " ,bgenre:"Poetry" ,byear:"January 23, 2016" ,bpicture:'assets/images/1.jpg'},
+    {bname:"The Secret" ,bauthor:" Rhonda Byrne",bgenre:"Self-help" , byear:"November 26, 2006" ,bpicture:'assets/images/2.jpg'},
+    {bname:"The Nectar Of Pain ", bauthor:"Najwa Zebain " ,bgenre:"Poetry,Fiction" ,byear:"October 10, 2016" ,bpicture:'assets/images/3.jpg'},
+    {bname:"Milk and honey ", bauthor:"Rupi Kaur " ,bgenre:"Poetry" ,byear:"March 20, 2016" ,bpicture:'assets/images/4.jpg'},
+]
